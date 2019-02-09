bbfba4122e56c054bf5ddb6eb5f54fbdc0f9fc93
Felipe Seolin Bento <bentof@alunos.utfpr.edu.br>
2019-02-09 01:27:52 -0200

    Solução rápida e preguiçosa para Galeria

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as bulmaCarousel from '../../../node_modules/bulma-extensions/bulma-carousel/dist/js/bulma-carousel.min.js';
 
 @Component({

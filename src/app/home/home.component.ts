745addb26e24dc6da4cfef1e6dcef0fcc8034af1
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-02-25 16:32:08 -0300

    Verificações no formulário

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  fieldNameInvalid = null;
+  fieldEmailInvalid = null;
+  fieldSubjectInvalid = null;
+  fieldMessageInvalid = null;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  fieldTouched(fieldName) {
+    switch (fieldName) {
+      case 'name':
+        this.fieldNameInvalid = true;
+        break;
+      case 'email':
+        this.fieldEmailInvalid = true;
+        break;
+      case 'subject':
+        this.fieldSubjectInvalid = true;
+        break;
+      case 'message':
+        this.fieldMessageInvalid = true;
+        break;
+    }
   }
 
 }

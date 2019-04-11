3f6137d1ad68e10d8c116aea7f0c703c3bb629af
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-04-11 08:12:20 -0300

    Melhorias

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,15 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  fieldNameInvalid = null;
-  fieldEmailInvalid = null;
-  fieldSubjectInvalid = null;
-  fieldMessageInvalid = null;
+  public fieldNameInvalid = null;
+  public fieldEmailInvalid = null;
+  public fieldSubjectInvalid = null;
+  public fieldMessageInvalid = null;
+
+  private FIELD_NAME = 'name';
+  private FIELD_EMAIL = 'email';
+  private FIELD_SUBJECT = 'subject';
+  private FIELD_MESSAGE = 'message';
 
   constructor() {
   }
@@ -19,18 +24,38 @@ export class HomeComponent implements OnInit {
     this.animateValue('students', 25, 100, 5000);
   }
 
-  fieldTouched(fieldName) {
+  public fieldTouched(fieldName: string) {
     switch (fieldName) {
-      case 'name':
-        this.fieldNameInvalid = true;
+      case this.FIELD_NAME:
+        // this.fieldNameInvalid = true;
+        this.verifyField(this.FIELD_NAME);
         break;
-      case 'email':
+      case this.FIELD_EMAIL:
         this.fieldEmailInvalid = true;
         break;
-      case 'subject':
+      case this.FIELD_SUBJECT:
         this.fieldSubjectInvalid = true;
         break;
-      case 'message':
+      case this.FIELD_MESSAGE:
+        this.fieldMessageInvalid = true;
+        break;
+    }
+  }
+
+  private verifyField(fieldName: string) {
+    switch (fieldName) {
+      case this.FIELD_NAME:
+        // if () {
+        //
+        // }
+        break;
+      case this.FIELD_EMAIL:
+        this.fieldEmailInvalid = true;
+        break;
+      case this.FIELD_SUBJECT:
+        this.fieldSubjectInvalid = true;
+        break;
+      case this.FIELD_MESSAGE:
         this.fieldMessageInvalid = true;
         break;
     }
--- src/app/sobre-nos/sobre-nos.component.ts
+++ src/app/sobre-nos/sobre-nos.component.ts
@@ -1,10 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import * as bulmaCarousel from '../../../node_modules/bulma-extensions/bulma-carousel/dist/js/bulma-carousel.min.js';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-sobre-nos',
   templateUrl: './sobre-nos.component.html',
-  styleUrls: ['./sobre-nos.component.css']
+  styleUrls: ['./sobre-nos.component.css'],
+  // Animação utilizada para fazer o efeito de fade in e out
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate(1000)),
+    ])
+  ]
 })
 export class SobreNosComponent implements OnInit {
 

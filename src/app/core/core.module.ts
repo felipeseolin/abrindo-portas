597531399ff781de11b908b3a6dded1f29801968
felipeseolin <felipe.seolin@hotmail.com>
2019-12-23 21:18:15 -0300

    New Input Component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FooterComponent} from './footer/footer.component';
 import {NavbarComponent} from './navbar/navbar.component';
+import { InputComponent } from './input/input.component';
 
 
 
@@ -9,6 +10,7 @@ import {NavbarComponent} from './navbar/navbar.component';
   declarations: [
     FooterComponent,
     NavbarComponent,
+    InputComponent,
   ],
   imports: [
     CommonModule
---
+++ src/app/core/input/input.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  @Input() isControl = true;
+  @Input() isRequired = true;
+  @Input() isDisabled = false;
+  @Input() isReadOnly = false;
+  @Input() type = 'text';
+  @Input() label = '';
+  @Input() placeholder = '';
+  @Input() color = '';
+  @Input() size = '';
+  @Input() iconRight = '';
+  @Input() iconLeft = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

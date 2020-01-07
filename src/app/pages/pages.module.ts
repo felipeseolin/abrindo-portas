32de5fc4fe371d8537cceb2cc7b9db800569b643
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-01-07 17:03:33 -0300

    New component Our-History

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,10 +4,12 @@ import {SharedModule} from '../shared/shared.module';
 
 import { HomeComponent } from './home/home.component';
 import {CoreModule} from '../core/core.module';
+import { OurHistoryComponent } from './our-history/our-history.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    OurHistoryComponent,
   ],
   imports: [
     CommonModule,

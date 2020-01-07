ce52bd55d530ccc55ff4619ef1dd012c9590dfea
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-01-07 17:46:32 -0300

    New Page Contact Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {OurHistoryComponent} from './pages/our-history/our-history.component';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
+import {ContactComponent} from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: 'history', component: OurHistoryComponent},
+  { path: 'contact', component: ContactComponent },
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: '**', component: NotFoundComponent }
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,12 +6,14 @@ import { HomeComponent } from './home/home.component';
 import {CoreModule} from '../core/core.module';
 import { OurHistoryComponent } from './our-history/our-history.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     OurHistoryComponent,
     NotFoundComponent,
+    ContactComponent,
   ],
   imports: [
     CommonModule,

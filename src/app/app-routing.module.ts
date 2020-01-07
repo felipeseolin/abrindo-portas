012ebe4424b256d7645c00d220acbcec52178bd4
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-01-07 17:31:18 -0300

    New PageNotFound Component Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {OurHistoryComponent} from './pages/our-history/our-history.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'history', component: OurHistoryComponent},
-  { path: '', component: HomeComponent, pathMatch: 'full'}
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,11 +5,13 @@ import {SharedModule} from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import {CoreModule} from '../core/core.module';
 import { OurHistoryComponent } from './our-history/our-history.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     OurHistoryComponent,
+    NotFoundComponent,
   ],
   imports: [
     CommonModule,

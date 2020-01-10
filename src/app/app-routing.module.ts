51390926c4feaeaac88f14b83de31b58db261d26
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-01-09 23:43:34 -0300

    Added About route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import {HomeComponent} from './pages/home/home.component';
 import {OurHistoryComponent} from './pages/our-history/our-history.component';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
 import {ContactComponent} from './pages/contact/contact.component';
+import {AboutComponent} from './pages/about/about.component';
 
 const routes: Routes = [
   { path: 'history', component: OurHistoryComponent},
   { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: '**', component: NotFoundComponent }
 ];

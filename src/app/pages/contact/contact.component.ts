600b3b63c61e5f815050666bce0e671eb47de2eb
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-01-09 23:29:43 -0300

    New Contact Page Info

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  nameInputValidation = function (value: string): string {
+    if (value && (value.trim().length === 0 || value.length < 5)) {
+      return 'O nome deve ser completo';
+    }
+    const regex = /^[a-zA-Z ]{2,30}$/;
+    if (!regex.test(value)) {
+      return 'O nome digitado não é válido';
+    }
+    return null;
+  };
+
+  emailInputValidation = function (value: string): string {
+    if (value.trim().length === 0) {
+      return 'É obrigatório inserir um endereço de e-mail';
+    }
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    if (!regex.test(value)) {
+      return 'Digite um endereço de e-mail válido';
+    }
+    return null;
+  };
+
+  subjectInputValidation = function (value: string): string {
+    if (value && value.trim().length === 0) {
+      return 'É obrigatório inserir um assunto';
+    }
+    if (value && value.length < 5) {
+      return 'O assunto deve conter ao menos 5 caracteres';
+    }
+    if (value && value.length > 50) {
+      return 'O assunto deve possuir menos de 50 caracteres';
+    }
+    return null;
+  };
+
+  messageInputValidation = function (value: string): string {
+    if (value && value.trim().length === 0) {
+      return 'É obrigatório inserir uma mensagem';
+    }
+    if (value && value.length > 1000) {
+      return 'A mensagem deve conter menos de 1000 caracteres';
+    }
+    return null;
+  };
+
   constructor() { }
 
   ngOnInit() {

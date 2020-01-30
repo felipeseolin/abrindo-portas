db2de87df11d68e185e83a69fb5eba61a1ea11a1
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-01-29 23:56:14 -0300

    Gallery Page with Presentation

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  private images = [
+    {src: '/assets/gallery/1.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/2.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/3.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/4.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/5.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/6.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/7.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/8.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/9.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/10.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/11.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/12.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/13.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/14.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/15.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/16.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/17.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/18.jpg', alt: 'Imagem'},
+    {src: '/assets/gallery/19.jpg', alt: 'Imagem'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
